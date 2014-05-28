23fe5c1726ecae8ebe9c20dd44025ef411debad7
liqian <liqian@no5media.com>
2014-05-28 14:25:59 +0800

    first commit

---
+++ src/com/bruce/designer/constants/ConstantKey.java
@@ -0,0 +1,9 @@
+package com.bruce.designer.constants;
+
+public interface ConstantKey {
+	
+	
+	public static final String BUNDLE_ALBUM_INFO_ID = "bundle_album_info_id";
+	public static final String BUNDLE_ALBUM_INFO = "bundle_album_info";
+
+}
